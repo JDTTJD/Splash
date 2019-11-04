48996f92c7323b0bd3aa03577fb7b1069685bf02
Lenovo <820231422@qq.com>
2019-11-04 17:00:02 +0800

    闪屏动画，尚未全屏

---
+++ app/src/main/java/com/tuoren/splash/FullScreenVideoView.java
@@ -0,0 +1,24 @@
+package com.tuoren.splash;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
+/**
+ * Create by JDT on 2019/11/4.
+ */
+
+public class FullScreenVideoView extends VideoView {
+    //主要用于这个直接new出来的对象
+    public FullScreenVideoView(Context context) {
+        super(context);
+    }
+    //主要用于xml文件中
+    public FullScreenVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FullScreenVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
--- app/src/main/java/com/tuoren/splash/SplashActivity.java
+++ app/src/main/java/com/tuoren/splash/SplashActivity.java
@@ -1,4 +1,41 @@
 package com.tuoren.splash;
 
-public class SplashActivity {
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+import java.io.File;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ *
+ *Create by JDT on 2019/11/4.
+ *
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mVideoView = (VideoView) findViewById(R.id.vv_play);
+        mVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + File.separator + R.raw.splash));
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+    }
 }
