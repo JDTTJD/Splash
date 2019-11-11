49e8c3553cebde8934e464aea7f88674791bf4df
Lenovo <820231422@qq.com>
2019-11-11 17:21:54 +0800

    完善ShanghaiFragment

--- app/src/main/java/com/tuoren/splash/main/shanghai/ShangHaiFragment.java
+++ app/src/main/java/com/tuoren/splash/main/shanghai/ShangHaiFragment.java
@@ -1,10 +1,14 @@
 package com.tuoren.splash.main.shanghai;
 
+import android.widget.TextView;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.tuoren.splash.R;
 import com.tuoren.splash.base.BaseFragment;
 import com.tuoren.splash.base.ViewInject;
 
-import androidx.fragment.app.Fragment;
+import butterknife.BindView;
 
 /**
  * Create by JDT on 2019/11/8.
@@ -12,8 +16,20 @@ import androidx.fragment.app.Fragment;
 
 @ViewInject(mainlayoutid = R.layout.fragment_shanghai)
 public class ShangHaiFragment extends BaseFragment {
+    @BindView(R.id.tv_shanghai_welcome)
+    TextView tvShanghaiWelcome;
+    @BindView(R.id.shanghai_collapsingtoolbarlayout)
+    CollapsingToolbarLayout shanghaiCollapsingtoolbarlayout;
+    @BindView(R.id.shanghai_app_barlayout)
+    AppBarLayout shanghaiAppBarlayout;
+
     @Override
     public void afterBindView() {
+        shanghaiAppBarlayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
 
+            }
+        });
     }
 }
